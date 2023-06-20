a4f50aab2ce6f7cbc9a5d2c6e92d4253f7d43442
Jean-f-05 <jean_f_05@hotmail.com>
2023-06-20 07:01:00 +0100

    comp: Hero & styles

---
+++ src/app/components/Hero/index.jsx
@@ -0,0 +1,28 @@
+import styles from "./styles.module.scss";
+import Button from "../Button/index";
+import Image from 'next/image'
+import blob from "../../../../public/blob5.png"
+
+const Hero = () => {
+    return (
+        <div className={styles.hero__wrapper}>
+            <div className={styles.hero__container}>
+                <h1 className={styles.hero__header}>Discover your true Self</h1>
+                {/*      <span className={styles.hero__header}>&</span> */}
+                <p className={styles.hero__paragraph}>Unlock your potential by completing the MBTI test</p>
+                <Button>Take the test!</Button>
+            </div>
+            {/* <div className={styles.hero__icons}>
+                <Image src="/explorerBg.png" height={100} width={100} alt='explorer icon'></Image>
+                <Image src="/healerBg.png" height={100} width={100} alt='healer icon'></Image>
+                <Image src="/sageBg.png" height={100} width={100} alt='sage icon'></Image>
+                <Image src="/commanderBg.png" height={100} width={100} alt='commander icon'></Image>
+
+            </div> */}
+            <Image src={blob} alt='blob' className={styles.hero__icon} height={450} width={450}></Image>
+
+        </div>
+    )
+}
+
+export default Hero;
