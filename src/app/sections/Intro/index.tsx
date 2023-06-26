a17059301ad8b42a24608c5f1db8f85dab61907f
Jean-f-05 <jean_f_05@hotmail.com>
2023-06-26 19:09:37 +0100

    comp: Intro section

---
+++ src/app/sections/Intro/index.tsx
@@ -0,0 +1,13 @@
+import IconsBar from '@/app/components/IconsBar'
+import styles from "./styles.module.scss"
+
+const Intro = () => {
+    return (
+        <section className={styles.intro__wrapper}>
+            <IconsBar />
+        </section>
+    )
+
+}
+
+export default Intro
