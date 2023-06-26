f1ff32a83db6feb331d7aa1121b65bac68a32e49
Jean-f-05 <jean_f_05@hotmail.com>
2023-06-26 19:09:01 +0100

    refactored components folder structure

--- src/app/sections/Hero/index.jsx
+++ src/app/sections/Hero/index.jsx
@@ -1,5 +1,5 @@
 import styles from "./styles.module.scss";
-import Button from "../Button/index";
+import Button from "../../components/Button/index";
 import Image from 'next/image'
 import blob from "../../../../public/blob5.png"
 
@@ -12,13 +12,6 @@ const Hero = () => {
                 <p className={styles.hero__paragraph}>Unlock your potential by completing the MBTI test</p>
                 <Button>Take the test!</Button>
             </div>
-            {/* <div className={styles.hero__icons}>
-                <Image src="/explorerBg.png" height={100} width={100} alt='explorer icon'></Image>
-                <Image src="/healerBg.png" height={100} width={100} alt='healer icon'></Image>
-                <Image src="/sageBg.png" height={100} width={100} alt='sage icon'></Image>
-                <Image src="/commanderBg.png" height={100} width={100} alt='commander icon'></Image>
-
-            </div> */}
             <Image src={blob} alt='blob' className={styles.hero__icon} height={450} width={450}></Image>
 
         </section>
